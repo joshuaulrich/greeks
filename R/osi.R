b3afc3c3cd9a4241d011a60548414e1a18bbc6df
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2010-12-29 15:28:16 +0000

    o  new optionsExp and nextExp docs/arg changes    likely that osi stuff will end up in FinancialInstrument    but still cleaning up here

--- R/osi.R
+++ R/osi.R
@@ -27,6 +27,11 @@ try.osi <- function(osi) {
 # osi print method
 print.osi <- function(x, ...) print(unclass(x))
 
+as.data.frame.osi <- function(x, ...) {
+  structure(list(x), row.names=.set_row_names(length(x)), class="data.frame")
+}
+
+
 expiry <- function(osi) {
   osi <- try.osi(osi)
   if(!is.osi(osi))
